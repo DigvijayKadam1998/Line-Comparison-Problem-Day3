774e4c009af1b79e3bb9185c6cab461424998a7d
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-07 14:05:17 +0530

    Added: Comparing Two Lines by using compare to method

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -2,48 +2,48 @@ package com.bridgelabz.linecomparison;
 
 import java.util.Scanner;
 public class LineComparison {
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-            //Variables for first line
-            int x1 , y1 , x2 , y2 ;
-            double LenthOfLine1, LenthOfLine2;
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        //Variables for first line
+        int x1 , y1 , x2 , y2 ;
+        double LenthOfLine1, LenthOfLine2;
+        // Points for line 1
+
+        System.out.println("Enter X1 Point");
+        x1 = sc.nextInt();
+        System.out.println("Enter Y1 Point");
+        y1 = sc.nextInt();
+        System.out.println("Enter X2 Point");
+        x2 = sc.nextInt();
+        System.out.println("Enter Y2 Point");
+        y2 = sc.nextInt();
+
+        //Computation for line 1
+        LenthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1));
         
-            // Points for line 1
-            System.out.println("Enter X1 Point");
-            x1 = sc.nextInt();
-            System.out.println("Enter Y1 Point");
-            y1 = sc.nextInt();
-            System.out.println("Enter X2 Point");
-            x2 = sc.nextInt();
-            System.out.println("Enter Y2 Point");
-            y2 = sc.nextInt();
+        // Variables for seconds line
+        int x3 , y3, x4 , y4 ;
+        // Points for line 2
+        System.out.println("Enter X3 Point");
+        x3 = sc.nextInt();
+        System.out.println("Enter Y3 Point");
+        y3 = sc.nextInt();
+        System.out.println("Enter X4 Point");
+        x4 = sc.nextInt();
+        System.out.println("Enter Y4 Point");
+        y4 = sc.nextInt();
+        // computation for line 2
+        LenthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3));
 
-            //Computation for line 1
-            LenthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1));
+        System.out.println(" Lenth Of Line 1 is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+"): " + LenthOfLine1);
+        System.out.println(" Lenth Of Line 2 is : "+"("+x3+","+y3+"),"+"("+x4+","+y4+"): " + LenthOfLine2);
         
-            // Variables for seconds line
-            int x3 , y3, x4 , y4 ;
-            // Points for line 2
-            System.out.println("Enter X3 Point");
-            x3 = sc.nextInt();
-            System.out.println("Enter Y3 Point");
-            y3 = sc.nextInt();
-            System.out.println("Enter X4 Point");
-            x4 = sc.nextInt();
-            System.out.println("Enter Y4 Point");
-            y4 = sc.nextInt();
-            // computation for line 2
-            LenthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3));
-
-            System.out.println(" Lenth Of Line 1 is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+"): " + LenthOfLine1);
-            System.out.println(" Lenth Of Line 2 is : "+"("+x3+","+y3+"),"+"("+x4+","+y4+"): " + LenthOfLine2);
-
-	    // check equality of two lines
-            if(LenthOfLine1==LenthOfLine2) {
-                System.out.println("Both Lines Are Equals");
-            }
-            else {
-                System.out.println("Both Lines Are Not Equals");
-            }
-       }
+        // comparing to length
+        if(LenthOfLine1==LenthOfLine2)
+            System.out.println("Line 1 and Line 2 are equals");
+        else if(LenthOfLine1 < LenthOfLine2)
+            System.out.println("Line 1 is less than Line 2");
+        else if(LenthOfLine1 > LenthOfLine2)
+            System.out.println("Line 1 gretter than Line 2");
+    }
 }
